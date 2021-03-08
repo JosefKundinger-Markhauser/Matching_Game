39640016606a106320283b1877e90d694396557a
Josef Kundinger-Markhauser <joeymarkhauser@yahoo.ca>
2021-03-08 18:19:30 -0500

    Created visual studio project
    Signed-off-by: Josef Kundinger-Markhauser <joeymarkhauser@yahoo.ca>

---
+++ Matching_Game.cpp
@@ -0,0 +1,12 @@
+﻿// Matching_Game.cpp : Defines the entry point for the application.
+//
+
+#include "Matching_Game.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
